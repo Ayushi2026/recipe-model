2cfa28628b05945ef93ccaa272c790b7139058a2
JW41BH <ayushi.trivedi@ing.com>
2024-06-17 16:55:07 +0200

    added controller and service

--- src/main/java/org/example/controller/RecipeController.java
+++ src/main/java/org/example/controller/RecipeController.java
@@ -1,4 +1,51 @@
 package org.example.controller;
 
+import org.example.model.Recipe;
+import org.example.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/recipes")
 public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping
+    public List<Recipe> getAllRecipes(){
+        return recipeService.getAllRecipes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> getRecipeById(@PathVariable Long id) {
+        Optional<Recipe> recipe = recipeService.getRecipeById(id);
+        if (recipe.isPresent()) {
+            return ResponseEntity.ok(recipe.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PostMapping
+    public Recipe addRecipe(@RequestBody Recipe recipe){
+        return  recipeService.addRecipe(recipe);
+    }
+
+    @GetMapping("/search")
+    public List<Recipe> searchRecipes(@RequestParam(required = false) String name,
+    @RequestParam(required = false) String ingredient){
+        if(name!= null) {
+            return recipeService.searchRecipesByName(name);
+        }
+        else if(ingredient!=null){
+            return  recipeService.searchRecipesByIngredient(ingredient);
+        }
+        else {
+            return  recipeService.getAllRecipes();
+        }
+    }
 }
--- src/main/java/org/example/service/RecipeService.java
+++ src/main/java/org/example/service/RecipeService.java
@@ -1,4 +1,34 @@
 package org.example.service;
 
+import org.example.model.Recipe;
+import org.example.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public List<Recipe> getAllRecipes(){
+        return recipeRepository.findAll();
+    }
+    public Optional<Recipe> getRecipeById(Long id){
+        return recipeRepository.findById(id);
+    }
+    public Recipe addRecipe(Recipe recipe)
+    {
+        return recipeRepository.save(recipe);
+    }
+
+    public List<Recipe> searchRecipesByName(String name){
+        return recipeRepository.findByNameContainingIgnoreCase(name);
+    }
+    public List<Recipe> searchRecipesByIngredient(String ingredient){
+        return recipeRepository.findByIngredientsContainingIgnoreCase(ingredient);
+    }
 }
